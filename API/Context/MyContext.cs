3458c3004099c6b69b711b63906a3fb65765b6b3
Fahri Hanif <fahrihanif03@gmail.com>
2023-04-06 10:19:49 +0700

    Initial commit

---
+++ API/Context/MyContext.cs
@@ -0,0 +1,13 @@
+﻿using API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Context;
+
+public class MyContext : DbContext
+{
+    public MyContext(DbContextOptions<MyContext> options) : base(options) { }
+
+    // Introduce the model to the database that eventually become an entity
+    public DbSet<Employee> Employees { get; set; }
+    public DbSet<Profiling> Profilings { get; set; }
+}
---
+++ API/Models/Profiling.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Models;
+
+[Table("tb_tr_profilings")]
+public class Profiling
+{
+    [Key, Column("employee_nik", TypeName = "char(5)")]
+    public string EmployeeNIK { get; set; }
+    [Column("education_id")]
+    public int EducationId { get; set; }
+}
